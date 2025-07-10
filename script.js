629ac36974dfc70474541ca4fbcc2ef8ea861df6
jnm2004 <jothika.mohan@kalkitech.com>
2025-07-10 17:02:02 +0530

    Javascript entered the chat

---
+++ script.js
@@ -0,0 +1,72 @@
+let box = document.getElementById("container");
+box.addEventListener("mouseover", function () {
+    box.style.backgroundColor = "rgb(252, 140, 140);";
+});
+box.addEventListener("mouseout", function () {
+    box.style.backgroundColor = "orange";
+});
+
+let cell1 = document.getElementById("cell1");
+cell1.addEventListener("mouseover", function () {
+    cell1.style.backgroundColor = "pink";
+});
+cell1.addEventListener("mouseout", function () {
+    cell1.style.backgroundColor = "lightblue";
+});
+let cell2 = document.getElementById("cell2");
+cell2.addEventListener("mouseover", function () {
+    cell2.style.backgroundColor = "yellow";
+});
+cell2.addEventListener("mouseout", function () {
+    cell2.style.backgroundColor = "red";
+});
+let cell3 = document.getElementById("cell3");
+cell3.addEventListener("mouseover", function () {
+    cell3.style.backgroundColor = "lightgreen";
+});
+cell3.addEventListener("mouseout", function () {
+    cell3.style.backgroundColor = "darkgreen";
+});
+let cell4 = document.getElementById("cell4");
+cell4.addEventListener("mouseover", function () {
+    cell4.style.backgroundColor = "pink";
+});
+cell4.addEventListener("mouseout", function () {
+    cell4.style.backgroundColor = "lightblue";
+});
+let cell5 = document.getElementById("cell5");
+cell5.addEventListener("mouseover", function () {
+    cell5.style.backgroundColor = "yellow";
+});
+cell5.addEventListener("mouseout", function () {
+    cell5.style.backgroundColor = "red";
+});
+let cell6 = document.getElementById("cell6");
+cell6.addEventListener("mouseover", function () {
+    cell6.style.backgroundColor = "lightgreen";
+});
+cell6.addEventListener("mouseout", function () {
+    cell6.style.backgroundColor = "darkgreen";
+});
+let cell7 = document.getElementById("cell7");
+cell7.addEventListener("mouseover", function () {
+    cell7.style.backgroundColor = "pink";
+});
+cell7.addEventListener("mouseout", function () {
+    cell7.style.backgroundColor = "lightblue";
+});
+let cell8 = document.getElementById("cell8");
+cell8.addEventListener("mouseover", function () {
+    cell8.style.backgroundColor = "yellow";
+});
+cell8.addEventListener("mouseout", function () {
+    cell8.style.backgroundColor = "red";
+});
+let cell9 = document.getElementById("cell9");
+cell9.addEventListener("mouseover", function () {
+    cell9.style.backgroundColor = "lightgreen";
+});
+cell9.addEventListener("mouseout", function () {
+    cell9.style.backgroundColor = "darkgreen";
+});
+
